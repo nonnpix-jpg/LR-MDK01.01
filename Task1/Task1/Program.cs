5d0c8c8db6e142c11c91756fe5f8d83ed58654d1
nonnpix-jpg <nastaaprokofevaa@gmail.com>
2026-09-12 11:02:20 +0300

    создание функции, считающей сумму цифр

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -9,13 +9,30 @@ namespace Task1
     class Program
     {
         private static int Input()
-        {
+        {   
+            
             Console.WriteLine("Введите натуральное число: ");
             int result = Convert.ToInt32(Console.ReadLine());
             return result;
         }
+
+        private static int Summ(int number)
+        {
+            int sum = 0;
+            int temp = number;
+
+            while (temp >0)
+            {
+                int digit = temp % 10;
+                sum += digit;
+                temp /= 10;
+            }
+            return sum;
+        }
+
         static void Main(string[] args)
         {
+
         }
     }
 }
