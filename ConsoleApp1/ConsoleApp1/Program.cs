346307a3d101961b9ebf9bf221ebe8c4f8c4dfa8
nonnpix-jpg <nastaaprokofevaa@gmail.com>
2026-09-12 10:42:19 +0300

    создане функции вода числа

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,25 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите три целых числа:");
-            Console.Write("Число 1:");
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Число 2:");
-            int b = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Число 3:");
-            int c = Convert.ToInt32(Console.ReadLine());
-
-            int max = a;
-            if (b > max)
-            {
-                max = b;
-            }
-            if (c > max)
-            {
-                max = c;
-            }
-            Console.WriteLine($"Максимальноре число: {max}");
-            Console.ReadKey();
+          
         }
     }
 }
---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        private static int Input()
+        {
+            Console.WriteLine("Введите натуральное число: ");
+            int result = Convert.ToInt32(Console.ReadLine());
+            return result;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
