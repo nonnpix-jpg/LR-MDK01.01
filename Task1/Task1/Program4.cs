82dc521ea9d9f58ed28c465bac0b68eac653ed3c
nonnpix-jpg <nastaaprokofevaa@gmail.com>
2026-09-12 12:54:09 +0300

    объединение всех задач в один файл

--- Task1/Task1/Program4.cs
+++ Task1/Task1/Program4.cs
@@ -11,6 +11,9 @@ namespace Task1
         static void Main(string[] args)
         {
             int a = SuperFunctions.Input();
+            Console.WriteLine($"Сумма цифр числа {a} равна {SuperFunctions.Summ(a)}");
+            Console.WriteLine($"Число с цифрами в обратном порядке: {SuperFunctions.Revers(a)}");
+            Console.WriteLine($"Количество цифр в числе {a} равно {SuperFunctions.Count(a)}");
             Console.WriteLine($"Произведение цифр числа {a} равно {SuperFunctions.Increase(a)}");
         }
     }
